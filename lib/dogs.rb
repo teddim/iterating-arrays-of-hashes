e3104987c5d528d9ffcad6fdca24f9afa6b3fb47
Teddi Maull <teddi@picturetheday.com>
2015-06-14 10:39:38 -0600

    some ruby hash practice

--- lib/dogs.rb
+++ lib/dogs.rb
@@ -29,5 +29,89 @@ class Dogs
   end
 
   # only edit below this line
+  def small_dogs
+    small = []
+    @dogs.each do |dog|
+      if dog[:size] == :small
+        small << dog
+      end
+    end
+    return small
+  end
 
+  def huge_dog
+    huge =[]
+    @dogs.each do |dog|
+      if dog[:size] == :huge
+        huge << {name: dog[:name]}
+      end
+    end
+    return huge
+  end
+
+  def large_dog_names
+    names = []
+    @dogs.each do |dog|
+      if dog[:size] == :large
+        names << dog[:name]
+      end
+    end
+    return names
+  end
+
+  def joes_large_dogs
+    joes_dogs = []
+    @dogs.each do |dog|
+      if dog[:size] == :large && dog[:owner][:name][:first] == "Joe"
+        joes_dogs << dog[:name]
+      end
+    end
+    return joes_dogs
+  end
+
+  def sizes
+    sizes = []
+    @dogs.each do |dog|
+      if sizes.index(dog[:size]) == nil
+        sizes << dog[:size]
+      end
+    end
+    return sizes
+  end
+
+  def owners
+    owners = []
+    @dogs.each do |dog|
+      full_name = "#{dog[:owner][:name][:first]} #{dog[:owner][:name][:last]}"
+      if owners.index(full_name) == nil
+        owners << full_name
+      end
+    end
+    return owners
+  end
+
+  def average_owners
+    owners = []
+    @dogs.each do |dog|
+      full_name = "#{dog[:owner][:name][:first]} #{dog[:owner][:name][:last]}"
+      if owners.index(full_name) == nil && dog[:owner][:owner_quality] == AVERAGE
+        owners << full_name
+      end
+    end
+    owners
+  end
+
+  def owner_dogs(owner)
+    owner_dogs = []
+    @dogs.each do |dog|
+      if dog[:owner][:name][:first] == owner
+        owner_dogs << dog[:name]
+      end
+    end
+    return owner_dogs
+  end
+
+  def to_s
+    
+  end
 end
